4c083b39cc6741ab0e7c70f2a5fcf501f12a9938
Anastasia Zimina <anastasba@gmail.com>
2021-04-12 19:28:38 -0700

    adding okta auth

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
+import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+  BrowserRouter as Router
 } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import { DashboardPage } from './pages/dashboard';
-import { UIPage } from './pages/ui';
-import { ProfilePage } from './pages/profile';
-import { ChatsPage } from './pages/chats';
-import { MessagesPage } from './pages/messages';
-import { TopNavComponent } from './components/top-navigation';
-import './App.css';
+import AppWithRouterAccess from './AppWithRouterAccess';
 
-function App() {
-  return (
-    <Router>
-      <div className="page-container">
-        <TopNavComponent />
-        <Container fluid="true" className="d-flex h-100 flex-column" >
-                {/* A <Switch> looks through its children <Route>s and
-                  renders the first one that matches the current URL. */}
-              <Switch>
-              <Route path="/ui">
-                <UIPage />
-              </Route>
-                <Route path="/profile">
-                  <ProfilePage />
-                </Route>
-                <Route path="/chats">
-                  <ChatsPage />
-                </Route>
-                <Route path="/messages">
-                  <MessagesPage />
-                </Route>
-                <Route path="/">
-                  <DashboardPage />
-                </Route>
-              </Switch>
-        </Container>
-      </div>
-    </Router>
-  );
-}
-
+const App = () => (
+  <Router>
+    <AppWithRouterAccess/>
+  </Router>
+);
 export default App;
---
+++ src/AppWithRouterAccess.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Route, useHistory, Switch } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import { useOktaAuth } from '@okta/okta-react';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { DashboardPage } from './pages/dashboard';
+import { UIPage } from './pages/ui';
+import { ProfilePage } from './pages/profile';
+import { ChatsPage } from './pages/chats';
+import { MessagesPage } from './pages/messages';
+import { TopNavComponent } from './components/top-navigation';
+import { LoginPage } from './pages/login';
+import './App.css';
+import { oktaAuthConfig, oktaSignInConfig } from './config';
+
+const oktaAuth = new OktaAuth(oktaAuthConfig);
+
+const AppWithRouterAccess = () => {
+  const history = useHistory();
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  };
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri, window.location.origin));
+  };
+
+  return (
+    <Security
+    oktaAuth={oktaAuth}
+    onAuthRequired={customAuthHandler}
+    restoreOriginalUri={restoreOriginalUri}
+  >
+      <div className="page-container">
+        <TopNavComponent />
+        <Container fluid="true" className="d-flex h-100 flex-column" >
+                {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+              <Switch>
+              <Route path="/ui">
+                <UIPage />
+              </Route>
+                < SecureRoute path="/profile">
+                  <ProfilePage />
+                </SecureRoute>
+                <SecureRoute path="/chats">
+                  <ChatsPage />
+                </SecureRoute>
+                <SecureRoute path="/messages">
+                  <MessagesPage />
+                </SecureRoute>
+                <SecureRoute path="/" exact={true}>
+                  <DashboardPage />
+                </SecureRoute>
+                <Route path="/login" render={() => <LoginPage config={oktaSignInConfig} />} />
+                <Route path='/login/callback' component={LoginCallback} />
+              </Switch>
+        </Container>
+      </div>
+    </Security>
+  );
+}
+
+export default AppWithRouterAccess;
