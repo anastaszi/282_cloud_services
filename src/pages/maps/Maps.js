fc037f6d8234b4b3e9aef617abf74e0d8b19835f
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2021-05-14 09:43:36 -0700

    give styling to maps

--- src/pages/maps/Maps.js
+++ src/pages/maps/Maps.js
@@ -1,29 +1,37 @@
 import React, { useState, useRef } from "react";
 import "./Maps.css";
 import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { Row, Col, Container, Jumbotron } from 'react-bootstrap'
 
 const Maps = (props) => {
-  // const [userLogin, setUserLogin] = useState(true);
+	// const [userLogin, setUserLogin] = useState(true);
 
-  const GoogleMapExample = withGoogleMap((props) => (
-    <GoogleMap
-      defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-      defaultZoom={13}
-    ></GoogleMap>
-  ));
-  return (
-    <>
-      <h1> Google Maps Here</h1>
-      <div>
-        <GoogleMapExample
-          containerElement={<div style={{ height: `500px`, width: "500px" }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+	const GoogleMapExample = withGoogleMap((props) => (
+		<GoogleMap
+			defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
+			defaultZoom={13}
+		></GoogleMap>
+	));
+	return (
+		<>
+			<Container fluid>
+				<h1> Google Maps Here</h1>
+			</Container>
 
-        <iframe src="https://www.atlistmaps.com/map/4bc972b7-76f6-4050-9293-f34c170475ee?share=true" allow="geolocation" width="100%" height="400px" frameborder="0" scrolling="no" allowfullscreen></iframe>
-      </div>
-    </>
-  );
+			<Container fluid>
+				<Row>
+					<Col lg={8}>
+						<iframe src="https://www.atlistmaps.com/map/4bc972b7-76f6-4050-9293-f34c170475ee?share=true" height="400px" width="100%" allow="geolocation" frameborder="0" scrolling="no" allowfullscreen></iframe>
+					</Col>
+					<GoogleMapExample
+						containerElement={<Col lg={4}></Col>}
+						mapElement={<div style={{ height: `400px` }} />}
+					/>
+
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default Maps;
